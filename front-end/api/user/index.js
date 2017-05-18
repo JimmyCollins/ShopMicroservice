8405f919301cf59730986667693141be872a9110
Jimmy <james.collins@intel.com>
2017-05-18 13:51:59 +0100

    Updated some log messages and removed unused code from user middleware functions in front-end service.

--- front-end/api/user/index.js
+++ front-end/api/user/index.js
@@ -187,7 +187,7 @@
 
             if (error !== null )
             {
-                console.log("Error deleting user: "+JSON.stringify(error));
+                console.log("Error making user administrator: "+JSON.stringify(error));
                 res.status(500).send('Internal server error!');
                 return;
             }
@@ -196,7 +196,7 @@
             {
                 if (body.error)
                 {
-                    console.log("Error deleting user: " + body.error);
+                    console.log("Error making user administrator: " + body.error);
                     res.status(500);
                     res.end();
                     return;
@@ -223,7 +223,7 @@
 
             if (error !== null )
             {
-                console.log("Error deleting user: "+JSON.stringify(error));
+                console.log("Error revoking user administrator privileges: "+JSON.stringify(error));
                 res.status(500).send('Internal server error!');
                 return;
             }
@@ -232,7 +232,7 @@
             {
                 if (body.error)
                 {
-                    console.log("Error deleting user: " + body.error);
+                    console.log("Error revoking user administrator privileges: " + body.error);
                     res.status(500);
                     res.end();
                     return;
@@ -243,136 +243,6 @@
 
         });
     });
-
-
-    app.get("/customers/:id", function(req, res, next) {
-        helpers.simpleHttpRequest(endpoints.customersUrl + "/" + req.session.customerId, res, next);
-    });
-    app.get("/cards/:id", function(req, res, next) {
-        helpers.simpleHttpRequest(endpoints.cardsUrl + "/" + req.params.id, res, next);
-    });
-
-    /*app.get("/customers", function(req, res, next) {
-        helpers.simpleHttpRequest(endpoints.customersUrl, res, next);
-    });*/
-    app.get("/addresses", function(req, res, next) {
-        helpers.simpleHttpRequest(endpoints.addressUrl, res, next);
-    });
-    app.get("/cards", function(req, res, next) {
-        helpers.simpleHttpRequest(endpoints.cardsUrl, res, next);
-    });
-    // Create Customer - TO BE USED FOR TESTING ONLY (for now)
-    app.post("/customers", function(req, res, next) {
-        var options = {
-            uri: endpoints.customersUrl,
-            method: 'POST',
-            json: true,
-            body: req.body
-        };
-
-        console.log("Posting Customer: " + JSON.stringify(req.body));
-
-        request(options, function(error, response, body) {
-            if (error) {
-                return next(error);
-            }
-            helpers.respondSuccessBody(res, JSON.stringify(body));
-        }.bind({
-            res: res
-        }));
-    });
-
-    // Create Address - TO BE USED FOR TESTING ONLY (for now)
-    app.post("/addresses", function(req, res, next) {
-        var options = {
-            uri: endpoints.addressUrl,
-            method: 'POST',
-            json: true,
-            body: req.body
-        };
-        console.log("Posting Address: " + JSON.stringify(req.body));
-        request(options, function(error, response, body) {
-            if (error) {
-                return next(error);
-            }
-            helpers.respondSuccessBody(res, JSON.stringify(body));
-        }.bind({
-            res: res
-        }));
-    });
-
-    // Create Card - TO BE USED FOR TESTING ONLY (for now)
-    app.post("/cards", function(req, res, next) {
-        var options = {
-            uri: endpoints.cardsUrl,
-            method: 'POST',
-            json: true,
-            body: req.body
-        };
-        console.log("Posting Card: " + JSON.stringify(req.body));
-        request(options, function(error, response, body) {
-            if (error) {
-                return next(error);
-            }
-            helpers.respondSuccessBody(res, JSON.stringify(body));
-        }.bind({
-            res: res
-        }));
-    });
-
-    // Delete Customer - TO BE USED FOR TESTING ONLY (for now)
-    app.delete("/customers/:id", function(req, res, next) {
-        console.log("Deleting Customer " + req.params.id);
-        var options = {
-            uri: endpoints.customersUrl + "/" + req.params.id,
-            method: 'DELETE'
-        };
-        request(options, function(error, response, body) {
-            if (error) {
-                return next(error);
-            }
-            helpers.respondSuccessBody(res, JSON.stringify(body));
-        }.bind({
-            res: res
-        }));
-    });
-
-    // Delete Address - TO BE USED FOR TESTING ONLY (for now)
-    app.delete("/addresses/:id", function(req, res, next) {
-        console.log("Deleting Address " + req.params.id);
-        var options = {
-            uri: endpoints.addressUrl + "/" + req.params.id,
-            method: 'DELETE'
-        };
-        request(options, function(error, response, body) {
-            if (error) {
-                return next(error);
-            }
-            helpers.respondSuccessBody(res, JSON.stringify(body));
-        }.bind({
-            res: res
-        }));
-    });
-
-    // Delete Card - TO BE USED FOR TESTING ONLY (for now)
-    app.delete("/cards/:id", function(req, res, next) {
-        console.log("Deleting Card " + req.params.id);
-        var options = {
-            uri: endpoints.cardsUrl + "/" + req.params.id,
-            method: 'DELETE'
-        };
-        request(options, function(error, response, body) {
-            if (error) {
-                return next(error);
-            }
-            helpers.respondSuccessBody(res, JSON.stringify(body));
-        }.bind({
-            res: res
-        }));
-    });
-
-    // Create Customer - TO BE USED FOR TESTING ONLY (for now)
-
 
     module.exports = app;
 }());
